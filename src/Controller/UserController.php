c1cf0392f86915d05b0ce5e3c03f8c8da92423e9
rayen <rayenchakroun25@gmail.com>
2025-02-19 11:15:09 +0100

    SubCatégorie system

---
+++ src/Controller/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class UserController extends AbstractController
+{
+    #[Route('/admin/user', name: 'app_user')]
+    public function index(UserRepository $userRepository ): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'users'=> $userRepository->findAll(),
+        ]);
+    }
+    #[Route('/admin/user/{id}/to/editor', name: 'app_user_to_editor')]
+    public function ChangeRole(User $user , EntityManagerInterface $entityManager): Response
+    {
+        $user->setRoles(["ROLE_EDITOR","ROLE_USER"]);
+        $entityManager->flush();
+        $this->addFlash("success","Le role éditeure a été ajouter a votre utilisateur");
+        return $this->redirectToRoute('app_user');
+    }
+    #[Route('/admin/user/{id}/remove/editor/role', name:'app_user_remove_editor_role')]
+    public function editRoleRemove (User $user , EntityManagerInterface $entityManager): Response
+    {
+        $user->setRoles(['']);
+        $entityManager->flush();
+        $this->addFlash('danger','Le role editeure a été supprimier a votre utilisateur');
+        return $this->redirectToRoute('app_user');
+    }
+    #[Route('/admin/user/{id}/remove/', name:'app_user_remove')]
+    public function userRemove(EntityManagerInterface $entityManager ,$id , UserRepository $userRepository): Response
+    {
+        $userFind=$userRepository->find($id);
+        $entityManager->remove($userFind);
+        $entityManager->flush();
+
+        $this->addFlash('danger','Votre utilisateur a été supprimé');
+        return $this->redirectToRoute('app_user');
+    }
+}
